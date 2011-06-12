defaa0a006bd229e20629a8e1771dcc7b5485fae
Andrew Crichton <andrew.g.crichton@gmail.com>
2011-06-12 16:07:39 -0400

    Competed the 3rd page of the tutorial.

--- urls.py
+++ urls.py
@@ -1,17 +1,12 @@
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls.defaults import *
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
+
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'djangotut.views.home', name='home'),
-    # url(r'^djangotut/', include('djangotut.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
+    (r'^polls/', include('polls.urls')),
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    (r'^admin/', include(admin.site.urls)),
 )
